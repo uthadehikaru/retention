eb57bbd4eaaf82ab970298640dfbb193abcc61e9
zuhriutama <zuhriutama@gmail.com>
2024-05-15 14:27:50 +0700

    bug fix route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,13 @@ Route::redirect('/login', 'admin/login')->name('login');
 Route::middleware('auth')->group(function() {
     Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
 });
+
+if(config('app.env')=='production'){
+    Livewire::setScriptRoute(function ($handle) {
+        return Route::get('/retention/public/livewire/livewire.js', $handle);
+    });
+
+    Livewire::setUpdateRoute(function ($handle) {
+        return Route::post('/retention/public/livewire/update', $handle);
+    });
+}
